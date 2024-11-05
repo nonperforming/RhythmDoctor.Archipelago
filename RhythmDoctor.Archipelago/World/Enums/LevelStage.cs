ba4691a1f6c959fe7e7374703afb362ef4fecab4
Non <43197300+nonperforming@users.noreply.github.com>
2024-11-06 12:47:53 +1300

    Internal to friendly name

---
+++ RhythmDoctor.Archipelago/World/Enums/LevelStage.cs
@@ -0,0 +1,225 @@
+namespace RhythmDoctor.Archipelago.World.Enums;
+
+public enum LevelStage
+{
+  #region Act 1 - Main Ward
+  /// <summary>
+  /// 1-1
+  /// </summary>
+  SamuraiTechno,
+  /// <summary>
+  /// 1-1N
+  /// </summary>
+  SamuraiDubstep,
+  /// <summary>
+  /// 1-2
+  /// </summary>
+  Intimate,
+  /// <summary>
+  /// 1-2N
+  /// </summary>
+  IntimateNight,
+  /// <summary>
+  /// 1-X
+  /// </summary>
+  BattlewornInsomniac,
+  /// <summary>
+  /// 1-CNY
+  /// </summary>
+  ChineseNewYear,
+  /// <summary>
+  /// 1-BOO
+  /// </summary>
+  ThemeOfReallySpookyBird,
+  #endregion
+
+  #region Act 2 - SVT Ward
+  /// <summary>
+  /// 2-1
+  /// </summary>
+  LofiHipHopBeatsToTreatPatientsTo,
+  /// <summary>
+  /// 2-1N
+  /// </summary>
+  WishICouldCareLess,
+  /// <summary>
+  /// 2-2
+  /// </summary>
+  SupraventricularTachycardia,
+  /// <summary>
+  /// 2-2N
+  /// </summary>
+  Unreachable,
+  /// <summary>
+  /// 2-3
+  /// </summary>
+  PuffPiece,
+  /// <summary>
+  /// 2-3N
+  /// </summary>
+  BombSniffingPomeranian,
+  /// <summary>
+  /// 2-4
+  /// </summary>
+  SongOfTheSea,
+  /// <summary>
+  /// 2-4N
+  /// </summary>
+  SongOfTheSeaNight,
+  /// <summary>
+  /// 2-X
+  /// </summary>
+  AllTheTimes,
+  /// <summary>
+  /// 2-B1
+  /// </summary>
+  BeansHopper,
+  #endregion
+
+  #region Act 3 - Main Ward
+  /// <summary>
+  /// 3-1
+  /// </summary>
+  SleepyGarden,
+  /// <summary>
+  /// 3-1N
+  /// </summary>
+  Lounge,
+  /// <summary>
+  /// 3-2
+  /// </summary>
+  Classy,
+  /// <summary>
+  /// 3-2N
+  /// </summary>
+  ClassyNight,
+  /// <summary>
+  /// 3-3
+  /// </summary>
+  DistantDuet,
+  /// <summary>
+  /// 3-3N
+  /// </summary>
+  DistantDuetNight,
+  /// <summary>
+  /// 3-X
+  /// </summary>
+  OneShiftMore,
+  #endregion
+
+  #region Act 4 - Train
+  /// <summary>
+  /// 4-1
+  /// </summary>
+  TrainingDoctorsTrainRidePerformance,
+  /// <summary>
+  /// 4-1N
+  /// </summary>
+  RollerdiscoRumble,
+  /// <summary>
+  /// 4-2
+  /// </summary>
+  Invisible,
+  /// <summary>
+  /// 4-2N
+  /// </summary>
+  InvisibleNight,
+  /// <summary>
+  /// 4-3
+  /// </summary>
+  Steinway,
+  /// <summary>
+  /// 4-3N
+  /// </summary>
+  SteinwayReprise,
+  /// <summary>
+  /// 4-4
+  /// </summary>
+  KnowYou,
+  /// <summary>
+  /// 4-4N
+  /// </summary>
+  Murmurs,
+  /// <summary>
+  /// 1-XN
+  /// Note that this stage is located in the Main Ward
+  /// </summary>
+  SuperBattlewornInsomniac,
+  #endregion
+
+  #region Act 5 - Physiotherapy Ward
+  /// <summary>
+  /// 5-1
+  /// </summary>
+  LuckyBreak,
+  /// <summary>
+  /// 5-1N
+  /// </summary>
+  OneSlipTooLate,
+  /// <summary>
+  /// 5-2
+  /// </summary>
+  LofiBeatsForPatientsToChillTo,
+  /// <summary>
+  /// 5-2N
+  /// </summary>
+  UnsustainableInconsolable,
+  /// <summary>
+  /// 5-3
+  /// </summary>
+  SeventhInningStretch,
+  /// <summary>
+  /// 5-B1
+  /// </summary>
+  RhythmWeightlifter,
+  /// <summary>
+  /// 5-X
+  /// </summary>
+  DreamsDontStop,
+  #endregion
+
+  #region Bonus - Basement
+  /// <summary>
+  /// X-FTS
+  /// </summary>
+  FixationsTowardTheStars,
+  /// <summary>
+  /// X-KOB
+  /// </summary>
+  KingdomOfBalloons,
+  /// <summary>
+  /// X-WOT
+  /// </summary>
+  WornOutTapes,
+  /// <summary>
+  /// X-MAT
+  /// </summary>
+  MeetAndTweet,
+  /// <summary>
+  /// MD-1
+  /// </summary>
+  BlackestLuxuryCar,
+  /// <summary>
+  /// MD-2
+  /// </summary>
+  TapeStopNight,
+  /// <summary>
+  /// MD-3
+  /// </summary>
+  // ReSharper disable once InconsistentNaming
+  The90sDecision,
+  /// <summary>
+  /// X-1
+  /// Moved from Art Room to Basement
+  /// </summary>
+  ArtExercise,
+  #endregion
+
+  #region Art Room
+  /// <summary>
+  /// X-0
+  /// Default end goal
+  /// </summary>
+  HelpingHands
+  #endregion
+}
