e62d0cb6e11fa751e7a43bfb7c10e63f62df8426
Non <43197300+nonperforming@users.noreply.github.com>
2024-11-14 14:25:43 +1300

    Update CustomLevelsWardUIHelper.cs

--- RhythmDoctor.Archipelago/Helpers/CustomLevelsWardUIHelper.cs
+++ RhythmDoctor.Archipelago/Helpers/CustomLevelsWardUIHelper.cs
@@ -2,20 +2,6 @@ namespace RhythmDoctor.Archipelago.Helpers;
 
 internal static class CustomLevelsWardUIHelper
 {
-  static GameObject TabTemplate = scnCLS.instance.transform.Find("Library Tab").gameObject;
-
-  // FIXME: Does this break on scene reloads? (Reference to GameObject is destroyed)
-  // Do we need to cache GameObject.Find in the first place? It should not be called often
-  //GameObject WardOptionsContainer
-  //{
-  //  get
-  //  {
-  //    if (_WardOptionsContainer == null) _WardOptionsContainer = GameObject.Find("WardOptions Container");
-  //  }
-  //}
-  //
-  //GameObject _WardOptionsContainer;
-
   internal static void CreateCustomTab(
     string label,
     int wardID,
@@ -24,10 +10,10 @@ internal static class CustomLevelsWardUIHelper
     AudioClip? selectAudio = null
   )
   {
+    Plugin.Logger.LogInfo($"Creating custom tab {label}: {wardID}");
     Assert.IsFalse(Enum.IsDefined(typeof(scnCLS.WardOptionName), wardID), "Cannot use existing WardOptionName");
-
-    GameObject tab = UnityEngine.Object.Instantiate(TabTemplate);
-    tab.transform.SetParent(scnCLS.instance.wardOptionsContainer.transform, false);
+    GameObject tabTemplate = GameObject.Find("Library Tab").gameObject;
+    GameObject tab = UnityEngine.Object.Instantiate(tabTemplate, scnCLS.instance.wardOptionsContainer.transform, false);
 
     GameObject signContainer = tab.transform.Find("LibrarySign Container").gameObject;
     GameObject signButton = signContainer.transform.Find("Button").gameObject;
@@ -57,7 +43,7 @@ internal static class CustomLevelsWardUIHelper
     scnCLS.WardOption wardOption = scnCLS.instance.wardOptions[0];
     wardOption.name = (scnCLS.WardOptionName)wardID;
 
-    scnCLS.instance.wardOptions.Add(wardOption);
+    scnCLS.instance.wardOptions.Add(wardOption); // FIXME: Silently failing, can't select ward option
     CustomLevelsWardUIPatch.LevelWardOptions.Add(wardID, action);
     // Register Action to run when user selected option
   }
