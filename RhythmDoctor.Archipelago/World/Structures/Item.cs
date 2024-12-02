625199607febc7888f9d6237384737e731f380cc
Non <43197300+nonperforming@users.noreply.github.com>
2024-12-02 17:01:02 +1300

    Use correct type for Items

--- RhythmDoctor.Archipelago/World/Structures/Item.cs
+++ RhythmDoctor.Archipelago/World/Structures/Item.cs
@@ -6,7 +6,7 @@ public struct Item
   public string Name;
 
   [YamlMember(Alias = "id")]
-  public ulong ID;
+  public long ID;
 
   [YamlMember(Alias = "classification")]
   public ItemClassification Classification;
