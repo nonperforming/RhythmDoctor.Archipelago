86a8a7525546b166d3858de549d74f8eb7a44782
Non <43197300+nonperforming@users.noreply.github.com>
2024-12-02 16:33:20 +1300

    Specify Locations by Area

--- RhythmDoctor.Archipelago/World/Structures/LocationsData.cs
+++ RhythmDoctor.Archipelago/World/Structures/LocationsData.cs
@@ -8,24 +8,5 @@ public struct LocationsData
     this = DataHelper.GetLocationsData();
   }
 
-  [YamlMember(Alias = "act-1")]
-  public Dictionary<LevelStage, List<Item>> Act1Levels;
-
-  [YamlMember(Alias = "act-2")]
-  public Dictionary<LevelStage, List<Item>> Act2Levels;
-
-  [YamlMember(Alias = "act-3")]
-  public Dictionary<LevelStage, List<Item>> Act3Levels;
-
-  [YamlMember(Alias = "act-4")]
-  public Dictionary<LevelStage, List<Item>> Act4Levels;
-
-  [YamlMember(Alias = "act-5")]
-  public Dictionary<LevelStage, List<Item>> Act5Levels;
-
-  [YamlMember(Alias = "basement")]
-  public Dictionary<LevelStage, List<Item>> BasementLevels;
-
-  [YamlMember(Alias = "art-room")]
-  public Dictionary<LevelStage, List<Item>> ArtRoomLevels;
+  public Dictionary<Area, Dictionary<LevelStage, List<Item>>> Levels;
 }
