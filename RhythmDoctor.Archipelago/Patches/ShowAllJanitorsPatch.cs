8d62e71de2163c7938f64e8675405c1d7215c89f
Non <43197300+nonperforming@users.noreply.github.com>
2024-12-06 19:11:29 +1300

    More efficient patch for Janitor

--- RhythmDoctor.Archipelago/Patches/ShowAllJanitorsPatch.cs
+++ RhythmDoctor.Archipelago/Patches/ShowAllJanitorsPatch.cs
@@ -6,11 +6,19 @@ namespace RhythmDoctor.Archipelago.Patches;
 [HarmonyPatch(typeof(scnLevelSelect))]
 internal static class ShowAllJanitorsPatch
 {
+  [HarmonyPatch(nameof(scnLevelSelect.PlaceJanitor))]
+  [HarmonyPrefix]
+  static void PlaceJanitorPatch(ref bool __runOriginal)
+  {
+    Plugin.Logger?.LogDebug("Bypassing PlaceJanitor");
+    __runOriginal = false;
+  }
+
   [HarmonyPatch(nameof(scnLevelSelect.HideJanitor))]
   [HarmonyPrefix]
   static void Prefix(ref bool __runOriginal)
   {
-    Plugin.Logger?.LogDebug("Bypassing HideJanitor");
+    Plugin.Logger?.LogWarning("Bypassing HideJanitor. This should never be called assuming PlaceJanitor was bypassed!");
     __runOriginal = false;
   }
 }
