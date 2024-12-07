3df608ecf260f1413c09b0eea8d881413147aac1
Non <43197300+nonperforming@users.noreply.github.com>
2024-12-07 20:35:40 +1300

    Remove ItemsData constructor
    This causes an infinite loop or bogus System.Exception when YamlDotNet tries to deserialize it.

--- RhythmDoctor.Archipelago/World/Structures/ItemsData.cs
+++ RhythmDoctor.Archipelago/World/Structures/ItemsData.cs
@@ -2,12 +2,6 @@ namespace RhythmDoctor.Archipelago.World.Structures;
 
 public struct ItemsData
 {
-  public ItemsData()
-  {
-    Plugin.Logger?.LogInfo("Creating ItemsData");
-    this = DataHelper.GetItemsData();
-  }
-
   [YamlMember(Alias = "levels")]
   public Dictionary<Ward, List<Item>> Levels;
 
