2b11305d0d29ca61a6b1a78b07a8540f65686f8a
Non <43197300+nonperforming@users.noreply.github.com>
2025-03-20 23:01:48 +1300

    Archipelago menu option patch

--- RhythmDoctor.Archipelago/Patches/Menu/LoadLoginMenuPatch.cs
+++ RhythmDoctor.Archipelago/Patches/Menu/LoadLoginMenuPatch.cs
@@ -1,12 +1,33 @@
 namespace RhythmDoctor.Archipelago.Patches.Menu;
 
 [HarmonyPatch(typeof(scnMenu))]
-internal static class LoadLoginMenuPatch
+internal static class ArchipelagoMenuOptionPatch
 {
-  [HarmonyPatch(nameof(scnMenu.Start))]
-  [HarmonyPostfix]
-  internal static void StartPostfix(scnMenu __instance)
+  private const string ARCHIPELAGO_OBJECT_NAME = "archipelago";
+
+  [HarmonyPatch(nameof(scnMenu.Awake))]
+  [HarmonyPrefix]
+  internal static void RenameMusicOption(ref scnMenu __instance)
   {
-    scnBase.GoToCustomLevelSelect();
+    GameObject labelObject = __instance.transform.Find("mainMenu/options/optionsContainer/music").gameObject;
+    labelObject.name = ARCHIPELAGO_OBJECT_NAME; // Otherwise this option can be hidden on Steam Deck
+    Text text = labelObject.GetComponent<Text>();
+    text.text = "Archipelago";
+  }
+
+  [HarmonyPatch(nameof(scnMenu.SelectOption))]
+  [HarmonyPrefix]
+  internal static void HandleArchipelagoOptionSelected(ref bool __runOriginal, ref scnMenu __instance)
+  {
+    bool archipelagoOptionSelected =
+      __instance.optionsText[__instance.currentOption].gameObject.name == ARCHIPELAGO_OBJECT_NAME;
+
+    if (archipelagoOptionSelected)
+    {
+      __runOriginal = false;
+      Plugin.ApplyArchipelagoMenuPatch();
+      __instance.PlayConfirmSound();
+      __instance.TransitionToScene("scnCLS");
+    }
   }
 }
