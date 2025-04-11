dee33b5355a8a1e804552bbb5041e8c5739807ed
Non <43197300+nonperforming@users.noreply.github.com>
2025-04-11 22:50:45 +1200

    Fix Nicole not dematerializing

--- RhythmDoctor.Archipelago/Patches/Gameplay/NicoleBlockagePatch.cs
+++ RhythmDoctor.Archipelago/Patches/Gameplay/NicoleBlockagePatch.cs
@@ -16,17 +16,10 @@ internal static class NicoleBlockagePatch
   {
     // We want to force this bool to false so Nicole doesn't tell us to go to 2-1N before entering the PT Ward:
     // bool flag = currentSelectableObject.id == "GoToAthleteWard" && !Persistence.GetLevelRank(Level.CareLess).passed;
-    return new CodeMatcher()
+    return new CodeMatcher(instructions)
       .MatchForward(true, new CodeMatch(OpCodes.Ldstr, "GoToAthleteWard"))
       .Advance(3) // Do not skip the check that the current selectable object's id is "GoToAthleteWard"
-      .SetOpcodeAndAdvance(OpCodes.Nop) // Delete all other instructions because we're going to return false regardless
-      .SetOpcodeAndAdvance(OpCodes.Nop)
-      .SetOpcodeAndAdvance(OpCodes.Nop)
-      .SetOpcodeAndAdvance(OpCodes.Nop)
-      .SetOpcodeAndAdvance(OpCodes.Nop)
-      .SetOpcodeAndAdvance(OpCodes.Nop)
-      .SetOpcodeAndAdvance(OpCodes.Nop)
-      .SetOpcodeAndAdvance(OpCodes.Nop)
+      .RemoveInstructions(8) // Delete all other instructions because we're going to return false regardless
       .SetOpcodeAndAdvance(OpCodes.Ldc_I4_0) // Replace ceq and push false (int32 0) onto stack
       .InstructionEnumeration();
   }
