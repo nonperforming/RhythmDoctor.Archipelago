daf32b49c9ee34b87ce68cb351898af5da302ca7
Non <43197300+nonperforming@users.noreply.github.com>
2025-04-25 19:38:00 +1200

    Remove outdated reference to CustomLevelsWardHelper

--- RhythmDoctor.Archipelago/Patches/Gameplay/JanitorPatch.cs
+++ RhythmDoctor.Archipelago/Patches/Gameplay/JanitorPatch.cs
@@ -8,8 +8,6 @@ static class JanitorPatch
 {
   /// <summary>
   /// Open the Archipelago Ward when interacting with the Janitor.
-  ///
-  /// <seealso cref="CustomLevelsWardHelper"/>
   /// </summary>
   /// <param name="__instance">Instance of <see cref="scnLevelSelect"/> that ran <see cref="scnLevelSelect.PerformEntityAction"/></param>
   /// <param name="__runOriginal">Whether to run the original method or not. This will be set to false if interacting with the Janitor.</param>
