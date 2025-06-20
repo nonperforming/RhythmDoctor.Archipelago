10a902f2094f524f7affae67deb21849e876c9be
Non <43197300+nonperforming@users.noreply.github.com>
2025-06-20 18:23:11 +1200

    Follow patch naming conventions for LogClearLevel

--- RhythmDoctor.Archipelago/Debug/Patches/LogClearLevelPatch.cs
+++ RhythmDoctor.Archipelago/Debug/Patches/LogClearLevelPatch.cs
@@ -2,11 +2,11 @@
 namespace RhythmDoctor.Archipelago.Debug.Patches;
 
 [HarmonyPatch(typeof(HUD))]
-internal static class LogClearLevelPatch
+static class LogClearLevelPatch
 {
   [HarmonyPatch(nameof(HUD.ShowAndSaveRank))]
   [HarmonyPrefix]
-  internal static void ShowAndSaveRank(bool bossLevelFailed = false, bool onlySavePersistence = false)
+  static void LogShowAndSaveRankPatch(bool bossLevelFailed, bool onlySavePersistence)
   {
     Plugin.Logger.LogInfo(
       @$"--- HUD.ShowAndSaveRank(bool bossLevelFailed = {bossLevelFailed}, bool onlySavePersistence = {onlySavePersistence})
@@ -28,7 +28,7 @@ Level ID: {scnGame.instance.levelIdentifier}
 
   [HarmonyPatch(typeof(HUD), nameof(HUD.AdvanceGameover))]
   [HarmonyPrefix]
-  internal static void AdvanceGameover(HUD __instance, bool isPlayer = false)
+  static void LogAdvanceGameoverPatch(HUD __instance, bool isPlayer)
   {
     Plugin.Logger.LogInfo(
       $@"--- HUD.AdvanceGameover(bool isPlayer = {isPlayer})
