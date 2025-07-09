762a401a0ca30640d39a0ba4ad56d8de0c792be1
Non <43197300+nonperforming@users.noreply.github.com>
2025-07-09 16:17:59 +1200

    Suppress duplicate debug menu creation message

--- RhythmDoctor.Archipelago/Debug/Patches/CreateDebugMenuPatch.cs
+++ RhythmDoctor.Archipelago/Debug/Patches/CreateDebugMenuPatch.cs
@@ -6,13 +6,18 @@ class CreateDebugMenuPatch
 {
   internal const string DEBUG_MENU_OBJECT_NAME = "RhythmDoctor.Archipelago Debug";
 
+  // We create the Debug Menu object here instead of creating it during plugin startup (before commit 5c838d37cc)
+  // as it seemingly breaks as of (Beta) build 18990163 (see https://steamdb.info/patchnotes/18990163/)
+  // (Start method does not run?)
   [HarmonyPatch(nameof(RDStartup.Setup))]
   [HarmonyPostfix]
   static void CreateDebugMenuAfterSetupPatch()
   {
     if (GameObject.Find($"/{DEBUG_MENU_OBJECT_NAME}") != null)
     {
-      Plugin.Logger.LogDebug("Debug menu object found, ignoring request to create new menu");
+      // Can get noisy, even seemingly innocent actions such as selecting entities in the Level Select calls
+      // RDStartup.Setup multiple times.
+      //Plugin.Logger.LogDebug("Debug menu object found, ignoring request to create new menu");
       return;
     }
 
