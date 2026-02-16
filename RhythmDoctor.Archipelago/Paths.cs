4674f8179b320d54668ce6dfd2bd60628a0cd2d5
Non <43197300+nonperforming@users.noreply.github.com>
2026-02-16 14:37:59 +1300

    Use Plugin.Info.Location for Paths

--- RhythmDoctor.Archipelago/Paths.cs
+++ RhythmDoctor.Archipelago/Paths.cs
@@ -2,13 +2,7 @@ namespace RhythmDoctor.Archipelago;
 
 internal static class Paths
 {
-  // TODO: Look into using Plugin.Info.Location
-  //       Having issues with a clean implementation, as PluginInfo is non-static.
-
-  // ReSharper disable once NullableWarningSuppressionIsUsed
-  internal static readonly string Assembly = Path.GetDirectoryName(
-    System.Reflection.Assembly.GetExecutingAssembly().Location
-  )!;
+  internal static readonly string Assembly = Path.GetDirectoryName(Plugin.Instance.Info.Location)!;
   internal static readonly string Assets = Path.Combine(Assembly, "Assets");
   internal static readonly string Localization = Path.Combine(Assets, "Localization");
   internal static readonly string WardIcons = Path.Combine(Assets, "WardIcons");
