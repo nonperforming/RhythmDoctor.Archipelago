fe6f772394fea9d2df010beca40a5947c7458a8e
Non <43197300+nonperforming@users.noreply.github.com>
2026-05-19 12:38:55 +1200

    Cleanup DisableSteamAchievementsPatch

--- RhythmDoctor.Archipelago/Debug/Patches/DisableSteamAchievementsPatch.cs
+++ RhythmDoctor.Archipelago/Debug/Patches/DisableSteamAchievementsPatch.cs
@@ -4,7 +4,7 @@ namespace RhythmDoctor.Archipelago.Debug.Patches;
 [HarmonyPatch(typeof(SteamIntegration))]
 internal static class DisableSteamAchievementsPatch
 {
-  [HarmonyPatch(nameof(SteamIntegration.UnlockAchievement), [typeof(string), typeof(bool)])]
+  [HarmonyPatch(nameof(SteamIntegration.UnlockAchievement), typeof(string), typeof(bool))]
   [HarmonyPrefix]
   private static void DoNotUnlockAchievementPatch(ref bool __runOriginal)
   {
